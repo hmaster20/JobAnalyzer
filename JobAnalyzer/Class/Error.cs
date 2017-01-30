0f38e6bf9806081a6ed836d740a267b3eef23dec
Sergei <hmaster20@gmail.com>
2017-01-30 14:14:59 +0300

    класс для обработки ошибок

---
+++ JobAnalyzer/Class/Error.cs
@@ -0,0 +1,32 @@
+﻿namespace JobAnalyzer
+{
+    class Err
+    {
+        public Err()
+        {
+            errors = new Errors[] { };
+            bad_arguments = new BadArguments[] { };
+        }
+
+        public Errors[] errors { get; set; }
+        public BadArguments[] bad_arguments { get; set; }
+
+        public string description { get; set; }
+        public string bad_argument { get; set; }
+    }
+
+    /// <summary>
+    /// Типовой класс ошибок HH (остальные официально не поддерживаются)
+    /// </summary>
+    class Errors
+    {
+        public string type { get; set; }
+        public string value { get; set; }
+    }
+
+    class BadArguments
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+    }
+}
