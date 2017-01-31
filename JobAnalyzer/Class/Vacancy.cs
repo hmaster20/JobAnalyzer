f13366a73f177430f3474c959cf1f0694c1a19eb
Sergei <hmaster20@gmail.com>
2017-01-31 10:38:14 +0300

    доработан класс

--- JobAnalyzer/Class/Vacancy.cs
+++ JobAnalyzer/Class/Vacancy.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 
 namespace JobAnalyzer
 {
-
-    //\"quick_responses_allowed\":true,\"negotiations_url\":null,\"department\":null,\"branded_description\":null,\"hidden\":false,\
-
     class Vacancy
     {
         public Vacancy()
@@ -25,16 +22,17 @@ namespace JobAnalyzer
         public string alternate_url { get; set; }
         public bool allow_messages { get; set; }
         public bool response_letter_required { get; set; }
+        public string apply_alternate_url { get; set; }
+        public bool quick_responses_allowed { get; set; }
+        public string negotiations_url { get; set; }
+        public string branded_description { get; set; }
+        public bool hidden { get; set; }
         public DateTime published_at { get; set; }
         public DateTime created_at { get; set; }
-
-
         public string city { get; set; }
-        public string apply_alternate_url { get; set; }
-        public Dictionary<string, string> department { get; set; }
-        public Dictionary<string, string> employment { get; set; } // Занятость
-        public Dictionary<string, string> area { get; set; } // здесь есть город
-
+        public Dictionary<string, string> department { get; set; }  // подразделение компании
+        public Dictionary<string, string> employment { get; set; }  // Занятость
+        public Dictionary<string, string> area { get; set; }        // здесь есть город
         public Key_skills[] key_skills { get; set; }
         public Schedule schedule { get; set; }
         public Experience experience { get; set; }
@@ -49,18 +47,14 @@ namespace JobAnalyzer
     /// </summary>
     class Employer
     {
-        public Dictionary<string, string> logo_urls { get; set; }
-        public string vacancies_url { get; set; }
-        public string name { get; set; }
-        public string url { get; set; }
-        public string alternate_url { get; set; }
-        public int id { get; set; }
+        public Dictionary<string, string> logo_urls { get; set; } // original Ссылка на логотип работодателя https://hhcdn.ru/employer-logo-original/246850.jpg\
+        public string vacancies_url { get; set; }   // ссылка на вакансии через API вида https://api.hh.ru/vacancies?employer_id=1733392\
+        public string name { get; set; }    // Наименование компании работодателя
+        public string url { get; set; }     // ссылка на информацию работодателя через API вида https://api.hh.ru/employers/1733392\
+        public string alternate_url { get; set; }   // ссылка на информацию работодателя через HH.ru вида https://hh.ru/employer/1733392\
+        public string original { get; set; }
+        public int id { get; set; }         // Идентификатор компании
         public bool trusted { get; set; }
-
-        //,\"employer\":{\"logo_urls\":{\"90\":\"https://hhcdn.ru/employer-logo/1430073.jpeg\",
-        //\"original\":\"https://hhcdn.ru/employer-logo-original/246850.jpg\",\"240\":\"https://hhcdn.ru/employer-logo/1430074.jpeg\"},                    
-        //"vacancies_url\":\"https://api.hh.ru/vacancies?employer_id=1733392\",\"name\":\"АВАРТ СИСТЕМС\",
-        //\"url\":\"https://api.hh.ru/employers/1733392\",\"alternate_url\":\"https://hh.ru/employer/1733392\",\"id\":\"1733392\",\"trusted\":true},
     }
 
     /// <summary>
@@ -68,30 +62,10 @@ namespace JobAnalyzer
     /// </summary>
     class Specializations
     {
-        public int profarea_id { get; set; }
-        public string profarea_name { get; set; }
         public float id { get; set; }
-        public string name { get; set; }
-
-        //\"specializations\":[
-        //{\"profarea_id\":\"1\",\"profarea_name\":\"Информационные технологии, интернет, телеком\",\"id\":\"1.221\",\"name\":\"Программирование, Разработка\"},
-        //{\"profarea_id\":\"1\",\"profarea_name\":\"Информационные технологии, интернет, телеком\",\"id\":\"1.272\",\"name\":\"Системная интеграция\"},
-        //{\"profarea_id\":\"1\",\"profarea_name\":\"Информационные технологии, интернет, телеком\",\"id\":\"1.161\",\"name\":\"Мультимедиа\"},
-        //{\"profarea_id\":\"1\",\"profarea_name\":\"Информационные технологии, интернет, телеком\",\"id\":\"1.82\",\"name\":\"Инженер\"},
-        //{\"profarea_id\":\"1\",\"profarea_name\":\"Информационные технологии, интернет, телеком\",\"id\":\"1.420\",\"name\":\"Администратор баз данных\"},
-        //{\"profarea_id\":\"25\",\"profarea_name\":\"Инсталляция и сервис\",\"id\":\"25.386\",\"name\":\"Инсталляция и настройка оборудования\"}]}"
-
-    }
-
-    /// <summary>
-    /// Доступность (открытость) вакансии
-    /// </summary>
-    class Type
-    {
-        public string id { get; set; }
-        public string name { get; set; }
-
-        //"type\":{\"id\":\"open\",\"name\":\"Открытая\"},
+        public string name { get; set; }        // Название: Программирование, Разработка
+        public int profarea_id { get; set; }
+        public string profarea_name { get; set; }   // Направление: Информационные технологии, интернет, телеком
     }
 
     /// <summary>
@@ -99,11 +73,18 @@ namespace JobAnalyzer
     /// </summary>
     class Salary
     {
-        public string to { get; set; }
-        public string from { get; set; }
-        public string currency { get; set; }
+        public string to { get; set; }          // null
+        public string from { get; set; }        //2000
+        public string currency { get; set; }    // USD
+    }
 
-        //"salary":{"to":null,"from":2000,"currency":"USD"}
+    /// <summary>
+    /// Доступность (открытость) вакансии
+    /// </summary>
+    class Type
+    {
+        public string id { get; set; }      // open
+        public string name { get; set; }    // Открытая
     }
 
     /// <summary>
@@ -132,16 +113,45 @@ namespace JobAnalyzer
         public string name { get; set; }
     }
 
-
     class Address
     {
+        public Address()
+        {
+            metro = new Metro();
+            metro_stations = new Metro_stations[] { };
+        }
+        public Metro metro { get; set; }
+        public Metro_stations[] metro_stations { get; set; }
 
-        //,"address\":
-        //{\"building\":\"46бк1\",\"city\":\"Москва\",\"description\":null,\
-        //"metro\":{\"line_name\":\"Таганско-Краснопресненская\",\"station_id\":\"7.139\",\"line_id\":\"7\",\"lat\":55.709211,\"station_name\":\"Текстильщики\",\"lng\":37.732117},
-        //\"metro_stations\":[{\"line_name\":\"Таганско-Краснопресненская\",\"station_id\":\"7.139\",\"line_id\":\"7\",\"lat\":55.709211,\"station_name\":\"Текстильщики\",\"lng\":37.732117}],"raw\":null,\"street\":\"Волгоградский проспект\",\"lat\":55.70461,\"lng\":37.726385},\  
+        public string building { get; set; }    // 46бк1
+        public string city { get; set; }    //Москва
+        public string description { get; set; }
+        public string street { get; set; }  // Волгоградский проспект
+        public float lat { get; set; }      // 55.709211
+        public float lng { get; set; }      // 37.732117
+
+        //,"raw\":null,\"       
     }
 
+    class Metro
+    {
+        public string line_name { get; set; } // Таганско-Краснопресненская
+        public float station_id { get; set; } // 7.139
+        public int line_id { get; set; }  // 7
+        public float lat { get; set; } // 55.709211
+        public string station_name { get; set; } // Текстильщики
+        public float lng { get; set; } // 37.732117
+    }
+
+    class Metro_stations : Metro
+    {
+        //public string line_name { get; set; } // Таганско-Краснопресненская
+        //public float station_id { get; set; } // 7.139
+        //public int line_id { get; set; }  // 7
+        //public float lat { get; set; } // 55.709211
+        //public string station_name { get; set; } // Текстильщики
+        //public float lng { get; set; } // 37.732117       
+    }
 
 
 }
