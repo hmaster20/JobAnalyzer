d7c43ef5b79eb71461ba699fdcda65504e7a3aa9
Sergei <hmaster20@gmail.com>
2017-02-01 17:59:25 +0300

    new

---
+++ JobAnalyzer/Class/VacancySearchCollection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace JobAnalyzer
+{
+    public class VacancyCollection
+    {
+        public VacancyCollection()
+        {
+            VacancyList = new List<Items>();
+        }
+
+        public void Clear()
+        {
+            try
+            {
+                ClearVacancyList();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        public static string BaseName { get; } = "VacancyList.xml";
+
+
+        public List<Items> VacancyList { get; set; }
+        public void Add(Items vac) => VacancyList.Add(vac);
+        public void Remove(Items vac) => VacancyList.Remove(vac);
+        public void ClearVacancyList() => VacancyList.Clear();
+
+
+
+
+        #region Сериализация
+
+        public void Save()                  // Сохранение
+        {
+            XmlSerializeHelper.SerializeAndSave(BaseName, this);
+        }
+
+        public static VacancyCollection Load()    // Загрузка
+        {
+            VacancyCollection result;
+            try
+            {
+                result = BaseName.LoadAndDeserialize<VacancyCollection>();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ошибка на этапе загрузки файла базы. \n" + ex.Message);
+            }
+            return result;
+        }
+
+        #endregion
+    }
+}
