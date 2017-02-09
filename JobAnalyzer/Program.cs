0597f312ddf849b49d8c7e71e6d4f844455efea7
Sergei <hmaster20@gmail.com>
2017-02-09 10:03:39 +0300

    .

--- JobAnalyzer/Program.cs
+++ JobAnalyzer/Program.cs
@@ -15,7 +15,7 @@ namespace JobAnalyzer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
