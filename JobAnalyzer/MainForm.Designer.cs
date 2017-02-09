bb2e753f3318f350e49751d5d54969546464be74
Sergei <hmaster20@gmail.com>
2017-02-09 10:01:46 +0300

    .

---
+++ JobAnalyzer/MainForm.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace JobAnalyzer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.dgvTable = new System.Windows.Forms.DataGridView();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnAnalyze = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lbKeys = new System.Windows.Forms.ListBox();
+            this.tbKey = new System.Windows.Forms.TextBox();
+            this.cbCountry = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbCity = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnRefreshCountry = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnGetArr = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(1005, 105);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnQuery.TabIndex = 0;
+            this.btnQuery.Text = "Query";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // dgvTable
+            // 
+            this.dgvTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTable.Location = new System.Drawing.Point(12, 12);
+            this.dgvTable.Name = "dgvTable";
+            this.dgvTable.Size = new System.Drawing.Size(779, 522);
+            this.dgvTable.TabIndex = 1;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(1005, 141);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnAnalyze
+            // 
+            this.btnAnalyze.Location = new System.Drawing.Point(989, 295);
+            this.btnAnalyze.Name = "btnAnalyze";
+            this.btnAnalyze.Size = new System.Drawing.Size(75, 23);
+            this.btnAnalyze.TabIndex = 3;
+            this.btnAnalyze.Text = "Анализ";
+            this.btnAnalyze.UseVisualStyleBackColor = true;
+            this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(827, 218);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(120, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lbKeys
+            // 
+            this.lbKeys.FormattingEnabled = true;
+            this.lbKeys.Location = new System.Drawing.Point(827, 273);
+            this.lbKeys.Name = "lbKeys";
+            this.lbKeys.Size = new System.Drawing.Size(120, 95);
+            this.lbKeys.TabIndex = 5;
+            this.lbKeys.DoubleClick += new System.EventHandler(this.lbKeys_DoubleClick);
+            // 
+            // tbKey
+            // 
+            this.tbKey.Location = new System.Drawing.Point(827, 247);
+            this.tbKey.Name = "tbKey";
+            this.tbKey.Size = new System.Drawing.Size(120, 20);
+            this.tbKey.TabIndex = 6;
+            // 
+            // cbCountry
+            // 
+            this.cbCountry.FormattingEnabled = true;
+            this.cbCountry.Location = new System.Drawing.Point(814, 37);
+            this.cbCountry.Name = "cbCountry";
+            this.cbCountry.Size = new System.Drawing.Size(121, 21);
+            this.cbCountry.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(811, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Страна";
+            // 
+            // cbCity
+            // 
+            this.cbCity.FormattingEnabled = true;
+            this.cbCity.Location = new System.Drawing.Point(814, 79);
+            this.cbCity.Name = "cbCity";
+            this.cbCity.Size = new System.Drawing.Size(121, 21);
+            this.cbCity.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(811, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Город";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(812, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Поисковое слово";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(814, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // btnRefreshCountry
+            // 
+            this.btnRefreshCountry.Image = global::JobAnalyzer.Properties.Resources.refresh;
+            this.btnRefreshCountry.Location = new System.Drawing.Point(941, 35);
+            this.btnRefreshCountry.Name = "btnRefreshCountry";
+            this.btnRefreshCountry.Size = new System.Drawing.Size(24, 23);
+            this.btnRefreshCountry.TabIndex = 2;
+            this.btnRefreshCountry.UseVisualStyleBackColor = true;
+            this.btnRefreshCountry.Click += new System.EventHandler(this.btnRefreshCountry_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(989, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Показать код";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnGetArr
+            // 
+            this.btnGetArr.Location = new System.Drawing.Point(827, 147);
+            this.btnGetArr.Name = "btnGetArr";
+            this.btnGetArr.Size = new System.Drawing.Size(75, 23);
+            this.btnGetArr.TabIndex = 11;
+            this.btnGetArr.Text = "районы";
+            this.btnGetArr.UseVisualStyleBackColor = true;
+            this.btnGetArr.Click += new System.EventHandler(this.btnGetArr_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1106, 546);
+            this.Controls.Add(this.btnGetArr);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbCity);
+            this.Controls.Add(this.cbCountry);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbKey);
+            this.Controls.Add(this.lbKeys);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnAnalyze);
+            this.Controls.Add(this.btnRefreshCountry);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dgvTable);
+            this.Controls.Add(this.btnQuery);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.DataGridView dgvTable;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnAnalyze;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lbKeys;
+        private System.Windows.Forms.TextBox tbKey;
+        private System.Windows.Forms.ComboBox cbCountry;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbCity;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnRefreshCountry;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnGetArr;
+    }
+}
+
