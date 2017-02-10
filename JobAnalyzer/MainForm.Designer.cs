b9a9a521c21bf95eb122876a1475bf5a55852551
Sergei <hmaster20@gmail.com>
2017-02-10 22:51:45 +0300

    изменение дизайна

--- JobAnalyzer/MainForm.Designer.cs
+++ JobAnalyzer/MainForm.Designer.cs
@@ -32,25 +32,26 @@
             this.dgvTable = new System.Windows.Forms.DataGridView();
             this.btnLoad = new System.Windows.Forms.Button();
             this.btnAnalyze = new System.Windows.Forms.Button();
-            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnAddKeyforAnalyze = new System.Windows.Forms.Button();
             this.lbKeys = new System.Windows.Forms.ListBox();
             this.tbKey = new System.Windows.Forms.TextBox();
             this.cbCountry = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbTextForQuery = new System.Windows.Forms.TextBox();
             this.btnRefreshCountry = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.treeView1 = new System.Windows.Forms.TreeView();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnRefreshRegion = new System.Windows.Forms.Button();
+            this.tbQuery = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.btnCheckTextQuery = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvTable)).BeginInit();
             this.SuspendLayout();
             // 
             // btnQuery
             // 
-            this.btnQuery.Location = new System.Drawing.Point(1134, 345);
+            this.btnQuery.Location = new System.Drawing.Point(1134, 413);
             this.btnQuery.Name = "btnQuery";
             this.btnQuery.Size = new System.Drawing.Size(75, 23);
             this.btnQuery.TabIndex = 0;
@@ -68,7 +69,7 @@
             // 
             // btnLoad
             // 
-            this.btnLoad.Location = new System.Drawing.Point(1134, 384);
+            this.btnLoad.Location = new System.Drawing.Point(1134, 439);
             this.btnLoad.Name = "btnLoad";
             this.btnLoad.Size = new System.Drawing.Size(75, 23);
             this.btnLoad.TabIndex = 2;
@@ -84,17 +85,16 @@
             this.btnAnalyze.TabIndex = 3;
             this.btnAnalyze.Text = "Анализ";
             this.btnAnalyze.UseVisualStyleBackColor = true;
-            this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click);
             // 
-            // btnAdd
+            // btnAddKeyforAnalyze
             // 
-            this.btnAdd.Location = new System.Drawing.Point(814, 384);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(120, 23);
-            this.btnAdd.TabIndex = 4;
-            this.btnAdd.Text = "Добавить";
-            this.btnAdd.UseVisualStyleBackColor = true;
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            this.btnAddKeyforAnalyze.Location = new System.Drawing.Point(814, 384);
+            this.btnAddKeyforAnalyze.Name = "btnAddKeyforAnalyze";
+            this.btnAddKeyforAnalyze.Size = new System.Drawing.Size(120, 23);
+            this.btnAddKeyforAnalyze.TabIndex = 4;
+            this.btnAddKeyforAnalyze.Text = "Добавить";
+            this.btnAddKeyforAnalyze.UseVisualStyleBackColor = true;
+            this.btnAddKeyforAnalyze.Click += new System.EventHandler(this.btnAddKeyforAnalyze_Click);
             // 
             // lbKeys
             // 
@@ -114,11 +114,13 @@
             // 
             // cbCountry
             // 
+            this.cbCountry.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbCountry.FormattingEnabled = true;
             this.cbCountry.Location = new System.Drawing.Point(814, 37);
             this.cbCountry.Name = "cbCountry";
             this.cbCountry.Size = new System.Drawing.Size(121, 21);
             this.cbCountry.TabIndex = 7;
+            this.cbCountry.SelectedIndexChanged += new System.EventHandler(this.cbCountry_SelectedIndexChanged);
             // 
             // label1
             // 
@@ -138,12 +140,12 @@
             this.label3.TabIndex = 9;
             this.label3.Text = "Поисковое слово";
             // 
-            // textBox1
+            // tbTextForQuery
             // 
-            this.textBox1.Location = new System.Drawing.Point(812, 259);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(121, 20);
-            this.textBox1.TabIndex = 6;
+            this.tbTextForQuery.Location = new System.Drawing.Point(812, 259);
+            this.tbTextForQuery.Name = "tbTextForQuery";
+            this.tbTextForQuery.Size = new System.Drawing.Size(291, 20);
+            this.tbTextForQuery.TabIndex = 6;
             // 
             // btnRefreshCountry
             // 
@@ -155,16 +157,6 @@
             this.btnRefreshCountry.UseVisualStyleBackColor = true;
             this.btnRefreshCountry.Click += new System.EventHandler(this.btnRefreshCountry_Click);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(971, 35);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 23);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Показать id страны";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // treeView1
             // 
             this.treeView1.Location = new System.Drawing.Point(813, 64);
@@ -173,23 +165,23 @@
             this.treeView1.TabIndex = 12;
             this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
             // 
-            // button2
+            // btnRefreshRegion
             // 
-            this.button2.Image = global::JobAnalyzer.Properties.Resources.refresh;
-            this.button2.Location = new System.Drawing.Point(1109, 64);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(24, 23);
-            this.button2.TabIndex = 2;
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.btnGetArr_Click);
+            this.btnRefreshRegion.Image = global::JobAnalyzer.Properties.Resources.refresh;
+            this.btnRefreshRegion.Location = new System.Drawing.Point(1109, 64);
+            this.btnRefreshRegion.Name = "btnRefreshRegion";
+            this.btnRefreshRegion.Size = new System.Drawing.Size(24, 23);
+            this.btnRefreshRegion.TabIndex = 2;
+            this.btnRefreshRegion.UseVisualStyleBackColor = true;
+            this.btnRefreshRegion.Click += new System.EventHandler(this.btnRefreshRegion_Click);
             // 
-            // textBox2
+            // tbQuery
             // 
-            this.textBox2.Location = new System.Drawing.Point(814, 303);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(266, 20);
-            this.textBox2.TabIndex = 6;
+            this.tbQuery.Location = new System.Drawing.Point(814, 303);
+            this.tbQuery.Name = "tbQuery";
+            this.tbQuery.ReadOnly = true;
+            this.tbQuery.Size = new System.Drawing.Size(266, 20);
+            this.tbQuery.TabIndex = 6;
             // 
             // label2
             // 
@@ -200,24 +192,45 @@
             this.label2.TabIndex = 9;
             this.label2.Text = "Подготовленный запрос";
             // 
+            // btnCheckTextQuery
+            // 
+            this.btnCheckTextQuery.Image = global::JobAnalyzer.Properties.Resources.refresh;
+            this.btnCheckTextQuery.Location = new System.Drawing.Point(1086, 301);
+            this.btnCheckTextQuery.Name = "btnCheckTextQuery";
+            this.btnCheckTextQuery.Size = new System.Drawing.Size(24, 23);
+            this.btnCheckTextQuery.TabIndex = 2;
+            this.btnCheckTextQuery.UseVisualStyleBackColor = true;
+            this.btnCheckTextQuery.Click += new System.EventHandler(this.btnCheckTextQuery_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(1116, 301);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(51, 23);
+            this.btnReset.TabIndex = 13;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1221, 546);
+            this.Controls.Add(this.btnReset);
             this.Controls.Add(this.treeView1);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cbCountry);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbQuery);
+            this.Controls.Add(this.tbTextForQuery);
             this.Controls.Add(this.tbKey);
             this.Controls.Add(this.lbKeys);
-            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnAddKeyforAnalyze);
             this.Controls.Add(this.btnAnalyze);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnCheckTextQuery);
+            this.Controls.Add(this.btnRefreshRegion);
             this.Controls.Add(this.btnRefreshCountry);
             this.Controls.Add(this.btnLoad);
             this.Controls.Add(this.dgvTable);
@@ -237,19 +250,20 @@
         private System.Windows.Forms.DataGridView dgvTable;
         private System.Windows.Forms.Button btnLoad;
         private System.Windows.Forms.Button btnAnalyze;
-        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnAddKeyforAnalyze;
         private System.Windows.Forms.ListBox lbKeys;
         private System.Windows.Forms.TextBox tbKey;
         private System.Windows.Forms.ComboBox cbCountry;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbTextForQuery;
         private System.Windows.Forms.Button btnRefreshCountry;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TreeView treeView1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnRefreshRegion;
+        private System.Windows.Forms.TextBox tbQuery;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCheckTextQuery;
+        private System.Windows.Forms.Button btnReset;
     }
 }
 
